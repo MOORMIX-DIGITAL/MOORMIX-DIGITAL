41d26d2b13b5ce74e38028d92e6b2a5e5d6934fa
MOORMIX DIGITAL <unredlopez@gmail.com>
2025-08-11 01:42:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const productos = [
     nombre: "Samsung Galaxy A06",
     precio: "$129.99",
     descripcion: "Smartphone con pantalla HD+, cámara dual y batería de larga duración.",
-    imagen: "imágenes/samsung-galaxy-a06.jpg"
+    imagen: "imagenes/samsung-galaxy-a06.jpg"
   },
   {
     nombre: "Auriculares Bluetooth MOORMIX",
